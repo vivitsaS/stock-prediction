959aeef2862611e19b2a54dc922ee1465d9d1cf9
vivitsaS <89176278+vivitsaS@users.noreply.github.com>
2022-04-16 18:28:52 +0530

    Add files via upload

---
+++ models/GRU.py
@@ -0,0 +1,27 @@
+# model definition
+# this model should take in train.size() number of 30*6 arrays
+
+import torch.nn as nn
+
+
+class GRUModel(nn.Module):
+    def __init__(self, d_feat=6, hidden_size=64, num_layers=2, dropout=0.0):
+        super().__init__()
+
+        self.rnn = nn.GRU(
+            input_size=d_feat,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+            dropout=dropout,
+        )
+        self.fc_out = nn.Linear(hidden_size, 1)
+
+        self.d_feat = d_feat
+
+    def forward(self, x):
+        # x: [N, F*T]
+        x = x.reshape(len(x), self.d_feat, -1)  # [N, F, T]
+        x = x.permute(0, 2, 1)  # [N, T, F]
+        out, _ = self.rnn(x)
+        return self.fc_out(out[:, -1, :]).squeeze()
